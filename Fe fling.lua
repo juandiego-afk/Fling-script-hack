851d29b5bfcfc4182c177c69a22dd723b010c89b
juandiego-afk <juandiegovazqueztorrealba@gmail.com>
2025-07-25 14:17:48 -0500

    Create Fe fling.lua
    Fling fe no fake

---
+++ Fe fling.lua
@@ -0,0 +1,7 @@
+echo "# Fe-fling-script-Io" >> README.md 
+git init 
+git add README.md 
+git commit -m "primer commit" 
+git branch -M main 
+git remote add origin https://github.com/juandiego-afk/Fe-fling-script-Io.git
+ git push -u origin main
